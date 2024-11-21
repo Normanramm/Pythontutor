b351b848de46b45c20b2e67955f202ae2d10cd9d
Vladislav <144459367+Normanramm@users.noreply.github.com>
2024-11-21 13:57:59 +0300

    Add files via upload

---
+++ Algebra.py
@@ -0,0 +1,15 @@
+# Выводим все четные и нечетные числа, разные варианты
+
+def is_even(x):
+    return x % 2 == 0
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = list(filter(is_even, numbers))
+print(even_numbers)  
+
+even_numbers_2 = list(filter(lambda x: x % 2 != 0, numbers))
+print(even_numbers_2)
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i,end=' ')
